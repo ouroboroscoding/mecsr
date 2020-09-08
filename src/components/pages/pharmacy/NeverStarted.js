2f956f20cd9b9d998677c26f72757c74200195ce
Chris Nasr <bast@maleexcel.com>
2020-09-08 10:27:14 -0400

    - Updated medefs - Added WD Never Started tab to Pharmacy page

--- src/components/pages/pharmacy/NeverStarted.js
+++ src/components/pages/pharmacy/NeverStarted.js
@@ -1,7 +1,7 @@
 /**
- * WellDyne Outbound
+ * WellDyne Never Started
  *
- * Created, edit, and delete outbound records
+ * Created, edit, and delete never/started records
  *
  * @author Chris Nasr <bast@maleexcel.com>
  * @copyright MaleExcelMedical
@@ -10,14 +10,17 @@
 
 // NPM modules
 import Tree from 'format-oc/Tree'
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 // Material UI
 import Box from '@material-ui/core/Box';
 import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import TextField from '@material-ui/core/TextField';
+import Tooltip from '@material-ui/core/Tooltip';
 
 // Material UI Icons
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 
 // Format Components
 import ResultsComponent from '../../format/Results';
@@ -31,35 +34,38 @@ import Tools from '../../../generic/tools';
 import Utils from '../../../utils';
 
 // Definitions
-import OutboundDef from '../../../definitions/welldyne/outbound';
-OutboundDef['__react__'] = {
-	"results": ["crm_id", "customer_name", "crm_order", "triggered", "queue", "reason", "ready"]
+import NeverStartedDef from '../../../definitions/welldyne/never_started';
+NeverStartedDef['__react__'] = {
+	"results": ["crm_id", "customer_name", "crm_order", "triggered", "reason", "ready"]
 }
-OutboundDef['customer_name'] = {"__type__": "string", "__react__": {"title": "Name"}}
-OutboundDef['triggered'] = {"__type__": "date"}
+NeverStartedDef['customer_name'] = {"__type__": "string", "__react__": {"title": "Name"}}
+NeverStartedDef['triggered'] = {"__type__": "date"}
 
 // Generate the Tree
-const OutboundTree = new Tree(OutboundDef);
+const NeverStartedTree = new Tree(NeverStartedDef);
 
 /**
- * Outbound
+ * NeverStarted
  *
  * Wraps common code that both tabs use
  *
  * @name Outreach
  * @extends React.Component
  */
-export default function Outbound(props) {
+export default function NeverStarted(props) {
 
 	// State
 	let [records, recordsSet] = useState(null);
+
+	// Refs
+	let pollRef = useRef();
 
 	// Effects
 	useEffect(() => {
 
 		// If we have a user with the correct rights
 		if(props.user) {
-			if(Utils.hasRight(props.user, 'welldyne_outbound', 'read')) {
+			if(Utils.hasRight(props.user, 'welldyne_never_started', 'read')) {
 				fetchRecords();
 			} else {
 				recordsSet(-1);
@@ -70,65 +76,11 @@ export default function Outbound(props) {
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [props.user]); // React to user changes
 
-	function adhocSwitch(_id) {
-
-		// Send the request to the service
-		Rest.update('welldyne', 'outbound/adhoc', {
-			"_id": _id
-		}).done(res => {
-
-			// If there's an error or a warning
-			if(res.error && !Utils.restError(res.error)) {
-				if(res.error.code === 1802) {
-					Events.trigger('error', 'No associated trigger can be found for this error, you will need to manually inform WellDyneRx to remove this.');
-				} else {
-					Events.trigger('error', JSON.stringify(res.error));
-				}
-			}
-			if(res.warning) {
-				if(res.warning === 1801) {
-					Events.trigger('warning', "Due to a lack of data this AdHoc can't be created automatically. You will be notifed as soon as it's created.");
-				} else {
-					Events.trigger('warning', JSON.stringify(res.warning));
-				}
-			}
-
-			// If there's data
-			if(res.data) {
-
-				// Use the current records to set the new records
-				recordsSet(records => {
-
-					// Clone the records
-					let ret = Tools.clone(records);
-
-					// Find the index
-					let iIndex = Tools.afindi(ret, '_id', _id);
-
-					// If one is found, remove it
-					if(iIndex > -1) {
-						ret.splice(iIndex, 1);
-					}
-
-					// Return the new records
-					return ret;
-				});
-
-				// If we got an object back
-				if(Tools.isObject(res.data)) {
-
-					// Tell the adhoc page there's a new record
-					Events.trigger('adhocCreated', res.data);
-				}
-			}
-		});
-	}
-
 	// Fetch all the records from the server
 	function fetchRecords() {
 
 		// Fetch all records
-		Rest.read('welldyne', 'outbounds', {}).done(res => {
+		Rest.read('welldyne', 'never/starteds', {}).done(res => {
 
 			// If there's an error or a warning
 			if(res.error && !Utils.restError(res.error)) {
@@ -145,6 +97,44 @@ export default function Outbound(props) {
 				recordsSet(res.data);
 			}
 		});
+	}
+
+	function getFlatDate() {
+		let oDate = new Date();
+		var Y = ('' + oDate.getFullYear()).substr(2,2);
+		var M = '' + (oDate.getMonth() + 1);
+		if(M.length === 1) M = '0' + M;
+		var D = '' + oDate.getDate();
+		if(D.length === 1) D = '0' + D;
+		return M + D + Y;
+	}
+
+	// Update the records on the server from the FTP
+	function pollRecords() {
+
+		Rest.update('welldyne', 'never/started/poll', {
+			date: pollRef.current.value
+		}).done(res => {
+
+			// If there's an error or a warning
+			if(res.error && !Utils.restError(res.error)) {
+				if(res.error.code === 1803) {
+					Events.trigger('error', "Can't find the file " + res.error.msg + ' on the FTP server');
+				} else {
+					Events.trigger('error', JSON.stringify(res.error));
+				}
+			}
+			if(res.warning) {
+				Events.trigger('warning', JSON.stringify(res.warning));
+			}
+
+			// If there's data
+			if(res.data) {
+
+				// Re-fetch the records
+				fetchRecords();
+			}
+		})
 	}
 
 	function readyRender(record) {
@@ -171,7 +161,7 @@ export default function Outbound(props) {
 		let bReady = event.currentTarget.checked
 
 		// Send the request to the service
-		Rest.update("welldyne", "outbound/ready", {
+		Rest.update("welldyne", "never/started/ready", {
 			"_id": sID,
 			"ready": bReady
 		}).done(res => {
@@ -208,28 +198,43 @@ export default function Outbound(props) {
 		})
 	}
 
+	// Remove a template
+	function removeRecord(_id) {
+
+		// Use the current records to set the new records
+		recordsSet(records => {
+
+			// Clone the records
+			let ret = Tools.clone(records);
+
+			// Find the index
+			let iIndex = Tools.afindi(ret, '_id', _id);
+
+			// If one is found, remove it
+			if(iIndex > -1) {
+				ret.splice(iIndex, 1);
+			}
+
+			// Return the new records
+			return ret;
+		});
+	}
+
 	// Figure out results
 	let results = ''
 	if(records === null) {
 		results = <div>Loading...</div>
 	} else if(records === -1) {
-		results = <div>You lack the rights to view Outbound records.</div>
+		results = <div>You lack the rights to view Never Started records.</div>
 	} else {
-		// If the user has both outbound delete, and adhoc create, all for
-		//	switching
-		let lActions = Utils.hasRight(props.user, 'welldyne_adhoc', 'create') ?
-						[{"tooltip": "AdHoc (Remove Error)", "icon": ArrowBackIcon, "callback": adhocSwitch}] :
-						[];
-
 		results = <ResultsComponent
-					actions={lActions}
 					custom={{"ready": readyRender}}
 					data={records}
-					noun="outbound"
+					noun="never/started"
 					orderBy="triggered"
-					remove={false}
+					remove={Utils.hasRight(props.user, 'welldyne_never_started', 'delete') ? removeRecord : false}
 					service="welldyne"
-					tree={OutboundTree}
+					tree={NeverStartedTree}
 					update={false}
 				/>
 	}
@@ -237,7 +242,21 @@ export default function Outbound(props) {
 	return (
 		<React.Fragment>
 			<Box className="pageHeader">
-				<div className="title">Outbound Failed Claims</div>
+				<div className="title">Never Started Claims</div>
+				{Utils.hasRight(props.user, 'welldyne_never_started', 'update') &&
+					<React.Fragment>
+						<TextField
+							inputRef={pollRef}
+							label="File date"
+							defaultValue={getFlatDate()}
+						/>
+						<Tooltip title="Update from FTP">
+							<IconButton onClick={pollRecords}>
+								<CloudDownloadIcon />
+							</IconButton>
+						</Tooltip>
+					</React.Fragment>
+				}
 			</Box>
 			{results}
 		</React.Fragment>

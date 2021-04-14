f37c48dc97a9588677701ad6bf0e60c6b30b945b
Chris Nasr <bast@maleexcel.com>
2021-04-14 09:54:15 -0400

    - At this point I am genuinely embarrassed of myself.

--- src/components/pages/Customer/Misc.js
+++ src/components/pages/Customer/Misc.js
@@ -230,7 +230,7 @@ function EVerify(props) {
 	function reSubmit() {
 
 		// Clear current results so it's obvious we are re-fetching
-		resultsSet(false);
+		resultsSet(null);
 
 		// Tell the server to update everify
 		Rest.update('monolith', 'customer/everify', {
